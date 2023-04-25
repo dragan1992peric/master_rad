f4509c5dd5849328aa5a763f5fcd3de19956fd3d
Dragan Peric <dragan1992peric@gmail.com>
2023-04-25 18:12:12 +0200

    dodata je pretraga, kao i sugestije, dosta za danas :D

--- VTSMASTER/Client/Services/ProductService/ProductService.cs
+++ VTSMASTER/Client/Services/ProductService/ProductService.cs
@@ -10,8 +10,9 @@ namespace VTSMASTER.Client.Services.ProductService
             _http = http;
         }
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products...";
 
-        public event Action ProductsChanged;
+		public event Action ProductsChanged;
 
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
 		{
@@ -29,5 +30,20 @@ namespace VTSMASTER.Client.Services.ProductService
 
             ProductsChanged.Invoke();
         }
-    }
+
+		public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+		{
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+		}
+
+		public async Task SearchProducts(string searchText)
+		{
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if(result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0) Message = "sipak bato moj";
+            ProductsChanged?.Invoke();
+		}
+	}
 }
