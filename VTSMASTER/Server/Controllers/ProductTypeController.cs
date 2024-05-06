1c8f3d9f3ecbf20c446a84b6a4447b458efbdbb5
Dragan Peric <dragan1992peric@gmail.com>
2024-05-06 18:05:26 +0200

    neradi jos uvek

---
+++ VTSMASTER/Server/Controllers/ProductTypeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VTSMASTER.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly IProductTypeService _productTypeService;
+
+        public ProductTypeController(IProductTypeService productTypeService)
+        {
+            _productTypeService = productTypeService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> GetProductTypes()
+        {
+            var responce = await _productTypeService.GetProductTypes();
+            return Ok(responce);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> AddProductType(ProductType productType)
+        {
+            var responce = await _productTypeService.AddProductType(productType);
+            return Ok(responce);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> UpdateProductType(ProductType productType)
+        {
+            var responce = await _productTypeService.UpdateProductType(productType);
+            return Ok(responce);
+        }
+    }
+}
--- VTSMASTER/Server/Program.cs
+++ VTSMASTER/Server/Program.cs
@@ -8,6 +8,7 @@ global using VTSMASTER.Server.Services.AuthService;
 global using VTSMASTER.Server.Services.OrderService;
 global using VTSMASTER.Server.Services.PaymentService;
 global using VTSMASTER.Server.Services.AddressService;
+global using VTSMASTER.Server.Services.ProductTypeService;
 global using Microsoft.IdentityModel.Tokens;
 global using Microsoft.AspNetCore.Authentication.JwtBearer;
 
@@ -36,6 +37,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     { 
